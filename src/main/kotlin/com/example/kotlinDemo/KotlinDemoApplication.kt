30e4bab47140a32f5320fa23f694ad4669bc6210
Kumar-Abhimanyu <kumar.abhimanyu.git@gmail.com>
2024-02-28 17:36:04 +0530

    KotlinCRUD

---
+++ src/main/kotlin/com/example/kotlinDemo/KotlinDemoApplication.kt
@@ -0,0 +1,61 @@
+package com.example.kotlinDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.web.bind.annotation.RequestBody
+import java.util.UUID
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.jdbc.core.query
+import org.springframework.web.bind.annotation.*
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import java.util.*
+
+@SpringBootApplication
+class KotlinDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinDemoApplication>(*args)
+}
+
+
+
+@RestController
+class MessageController(val service: MessageService) {
+	@GetMapping("/")
+	fun index(): List<Message> = service.findMessages()
+
+	@GetMapping("/{id}")
+	fun index(@PathVariable id: String): List<Message> =
+		service.findMessageById(id)
+
+	@PostMapping("/")
+	fun post(@RequestBody message: Message) {
+		service.save(message)
+	}
+}
+
+@Table("MESSAGES")
+data class Message(@Id var id: String?, val text: String)
+
+@Service
+class MessageService(val db: MessageRepository) {
+	fun findMessages(): List<Message> = db.findAll().toList()
+
+	fun findMessageById(id: String): List<Message> = db.findById(id).toList()
+
+	fun save(message: Message) {
+		db.save(message)
+	}
+
+	fun <T : Any> Optional<out T>.toList(): List<T> =
+		if (isPresent) listOf(get()) else emptyList()
+}
+
+interface MessageRepository : CrudRepository<Message, String>
